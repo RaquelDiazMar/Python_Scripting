190f2b187e975ce5457cd335d68fc3cc74a07577
R Diaz <54184336+RaquelDiazMar@users.noreply.github.com>
2021-09-26 10:31:34 +0200

    Update Saving_url_content_to_a_file.py

--- Saving_url_content_to_a_file.py
+++ Saving_url_content_to_a_file.py
@@ -6,7 +6,9 @@ def saveUrlContent():
     content = urllib.request.urlopen(url)
     data = content.read()
     with open('urlData.html', 'wb') as file: 
-    #if websiteData.html does not exist yet, it will be created, otherwise, it will be overwritten
+    #Note1: if websiteData.html does not exist yet, it will be created, otherwise, it will be overwritten
+    #Note2: the open() function opens a file in text format by default. To open a file in binary format, add 'b' 
+    #to the mode parameter. Hence the "wb" mode opens the file in binary format for writing.  
         file.write(data)
                 
 saveUrlContent()
@@ -16,7 +18,7 @@ saveUrlContent()
 def saveAnyUrlContent(url):
     content = urllib.request.urlopen(url)
     data = content.read()
-    with open('urlData.html', 'wb') as file: #if websiteData.html does not exist yet, it will be created, otherwise, it will overwrite it
+    with open('urlData.html', 'wb') as file:
         file.write(data)
 
 saveAnyUrlContent("https://www.google.com")
