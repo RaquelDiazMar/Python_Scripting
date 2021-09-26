ed2d51087f60b378bcd5d67d21f866e4c9efbe92
R Diaz <54184336+RaquelDiazMar@users.noreply.github.com>
2021-09-26 13:01:01 +0200

    Create Finding_words_in_a_string.py

---
+++ Finding_words_in_a_string.py
@@ -0,0 +1,14 @@
+#[Python Regular Expression Operations](https://docs.python.org/3/library/re.html)
+import re
+
+str = "on the internet you can learn programming and also help others to learn"
+word = "internet"
+
+def findWord(word, str):
+  content = re.findall(word, str)
+  print(content)
+  
+  
+findWord(word, content)
+
+####################################################
